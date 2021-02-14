e1930442dbdf3b99a9002406778bb17c8883d6dd
Sudhanshu <danshu.ds@gmail.com>
2021-02-14 22:34:53 +0530

    final commit

---
+++ faultyCalculator.py
@@ -0,0 +1,26 @@
+print("Welcome to my fault calculator ")
+Digit_one = int(input("enter first digit"))
+Digit_two = int(input("enter second digit"))
+Operator = str(input("enter operator"))
+result =0
+if Operator =='+':
+    if Digit_one == 56 and Digit_two == 9:
+        print(77)
+    else:
+        print(Digit_one + Digit_two)
+if Operator =='-':
+    if Digit_one == 56 and Digit_two == 9:
+        print(77)
+    else:
+        print(Digit_one - Digit_two)
+if Operator =='*':
+    if Digit_one == 56 and Digit_two == 9:
+        print(77)
+    else:
+        print(Digit_one * Digit_two)
+if Operator =='/':
+    if Digit_one == 56 and Digit_two == 9:
+        print(77)
+    else:
+        print(Digit_one / Digit_two) 
+   
